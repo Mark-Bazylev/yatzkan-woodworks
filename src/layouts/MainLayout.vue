3707bf63400986456961c932b09481a5171738d8
Mark-Bazylev <138299775+Mark-Bazylev@users.noreply.github.com>
2023-11-23 20:37:04 +0200

    chore: initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-layout view="lHr Lpr lFr">
+    Hello
+  <q-header>Hi</q-header>
+    <q-drawer>ya</q-drawer>
+    <q-page-container>
+      <q-page>
+        <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci commodi dolore eaque quidem sit. Architecto dolor doloribus esse et ex explicabo impedit maiores sunt tempora voluptas? Excepturi in minima molestiae?</h1>
+      </q-page>
+    </q-page-container>
+    <q-footer>footer</q-footer>
+
+  </q-layout>
+</template>
